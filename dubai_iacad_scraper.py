1f9c87237cb8afb12cf79e10bbe25d5c25ec5eed
Mahmood Mahmood <mahmood.a.yahya@gmail.com>
2021-12-31 22:30:39 +0400

    refactored dubai iacad scraper to be a class

--- dubai_iacad_scraper.py
+++ dubai_iacad_scraper.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_prayer_time_page():
-    URL = "https://services.iacad.gov.ae/SmartPortal/Timings"
-    headers = requests.structures.CaseInsensitiveDict()
-    headers["Cookie"] = "_culture=en-GB"
-    return requests.get(URL, headers=headers)
+class DubaiIacadPrayerTimes:
+    def __init__(self):
+        self.URL = "https://services.iacad.gov.ae/SmartPortal/Timings"
+        self.headers = requests.structures.CaseInsensitiveDict()
+        self.headers["Cookie"] = "_culture=en-GB"
 
-def get_prayer_times_from_page(page):
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="dt2")
+    def get_athan_times_today(self):
+        page = self._get_prayer_time_page()
+        return self._get_prayer_times_from_page(page)
 
-    prayer_names_arr =  [child.text.strip() for child in results.find_all(attrs={"data-title":"Pray name"})]
-    prayer_times_arr = [child.text.strip() for child in results.find_all(attrs={"data-title":"Time"})]
+    def _get_prayer_time_page(self):
+        return requests.get(self.URL, headers=self.headers)
 
-    return dict(zip(prayer_names_arr, prayer_times_arr))
+    def _get_prayer_times_from_page(self, page):
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id="dt2")
 
-page = get_prayer_time_page()
-print(get_prayer_times_from_page(page))
+        prayer_names_arr =  [child.text.strip() for child in results.find_all(attrs={"data-title":"Pray name"})]
+        prayer_times_arr = [child.text.strip() for child in results.find_all(attrs={"data-title":"Time"})]
+
+        return dict(zip(prayer_names_arr, prayer_times_arr))
+
+if __name__ == "__main__":
+    times = DubaiIacadPrayerTimes()
+    print(times.get_athan_times_today())
